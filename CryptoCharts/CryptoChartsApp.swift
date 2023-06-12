54001526e15266bec282e6499c64bd92c903d6ab
Kacper Biaduń <kacperbiadun.12@gmail.com>
2023-06-12 23:24:19 +0200

    Initial Commit

---
+++ CryptoCharts/CryptoChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoChartsApp.swift
+//  CryptoCharts
+//
+//  Created by Kacper Biaduń on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
